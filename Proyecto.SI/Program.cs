3d794f7edf4b542aaf702cdbffc81673f4ab5fd6
Osvaldo Ramirez <123326455+Osvi22@users.noreply.github.com>
2025-11-26 02:25:20 -0600

    Update Program.cs

--- Proyecto.SI/Program.cs
+++ Proyecto.SI/Program.cs
@@ -3,32 +3,34 @@ using Newtonsoft.Json;
 using Proyecto.BL;
 using Proyecto.DA;
 using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.Mvc.NewtonsoftJson; 
-
+using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Controllers + Json config
 builder.Services.AddControllers()
     .AddNewtonsoftJson(options =>
     {
         options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
     });
 
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DBContexto>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<IPersonaRepository, Proyecto.DA.PersonaRepository>();
-builder.Services.AddScoped<IAdministradorDePersonas, Proyecto.BL.AdministradorDePersonas>();
-builder.Services.AddScoped<IVehiculoRepository, Proyecto.DA.VehiculoRepository>();
-builder.Services.AddScoped<IAdministracionDeVehiculo, Proyecto.BL.AdministracionDeVehiculo>();
+// Repositorios y servicios
+builder.Services.AddScoped<IPersonaRepository, PersonaRepository>();
+builder.Services.AddScoped<IAdministradorDePersonas, AdministradorDePersonas>();
 
+builder.Services.AddScoped<IVehiculoRepository, VehiculoRepository>();
+builder.Services.AddScoped<IAdministracionDeVehiculo, AdministracionDeVehiculo>();
 
 var app = builder.Build();
 
-// HABILITAR SWAGGER EN PRODUCCIÓN (AZURE)
+// Swagger habilitado en producción
 app.UseSwagger();
 app.UseSwaggerUI(options =>
 {
@@ -38,7 +40,6 @@ app.UseSwaggerUI(options =>
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
 
 app.MapControllers();
 
